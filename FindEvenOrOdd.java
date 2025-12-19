03e6e2b38f708166da78e5f95c41f1658523e393
rupeshsawantcv <rupeshsawantcv@gmail.com>
2025-12-19 09:48:50 +0530

    Create FindEvenOrOdd.java

---
+++ FindEvenOrOdd.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class FindEvenOrOdd {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a number: ");
+       
+       int num = sc.nextInt();
+       System.out.println(num);
+       
+       if(num%2==0){
+           System.out.println("The Number is even: "+num);
+       }else{
+           System.out.println("The Number is Odd: "+num);
+       }
+    }
+}
